1b12209ff88088e44e3c29e22210b15574258fd9
Kyriakos321 <77235701+Kyriakos321@users.noreply.github.com>
2022-08-05 17:30:42 +0100

    Add files via upload

---
+++ dsa-cwk-1-session-2122-resit-template-Kyriakos321/src/view/aView.java
@@ -0,0 +1,17 @@
+package view;
+
+import helpers.OutputHelper;
+import model.lettersASCII;
+
+import java.util.ArrayList;
+
+public class aView {
+    public void displayAnItem(lettersASCII anItem){
+        // Add your code here
+    }
+
+    public void displayData(ArrayList<lettersASCII> theData){
+        // Add your code here
+    }
+
+}
